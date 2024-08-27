c1e19177b2c16d9510bf45d749c379601e847d32
ATHIRA <ATHIRA@LAPTOP-BNJ2H6K5>
2024-08-27 21:46:34 +0530

    new commit

--- training/src/main/java/com/obsquare1/training/ExcelRead.java
+++ training/src/main/java/com/obsquare1/training/ExcelRead.java
@@ -1,24 +1,38 @@
 package com.obsquare1.training;
 
-import java.io.File;
 import java.io.FileInputStream;
-import org.apache.poi.hssf.eventusermodel.*;
-import org.apache.poi.common.usermodel.*;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelRead{
-	public static void main(String[] args) {
-		File file=new File("C:\\Users\\ATHIRA\\OneDrive\\Documents\\data.xlsx");
-		FileInputStream fis=new FileInputStream(file);
-		XSSFWorkbook workbook=new XSSFWorkbook(fis);
-		XSSFSheet sheet=workbook.getsheetAt(0);
-		String CellValue=sheet.getRow(0).getCell(0).getStringCellValue();
-;
-		System.out.println(CellValue);
-		workbook.close();
-		fis.close();
+
+	static FileInputStream fis;
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	public static String readStringData(int row,int column) throws Exception{
+	fis =new FileInputStream("C:\\Users\\ATHIRA\\OneDrive\\Documents\\data.xlsx");
+	workbook= new XSSFWorkbook(fis);
+	sheet =workbook.getSheet("Sheet1");
+	XSSFRow r=sheet.getRow(row);
+	XSSFCell c= r.getCell(column);
+	return c.getStringCellValue();
 	}
-	
-	
+	public static double readNumericData(int row,int column) throws Exception{
+		fis=new FileInputStream("C:\\Users\\ATHIRA\\OneDrive\\Documents\\data.xlsx");
+		workbook = new XSSFWorkbook(fis);
+		sheet=workbook.getSheet("Sheet1");
+		XSSFRow r= sheet.getRow(row);
+		XSSFCell c= r.getCell(column);
+		return c.getNumericCellValue();
+	}
+	public static void main(String[] args) throws Exception  {
+	System.out.println(ExcelRead.readStringData(0, 0));
+	double d=ExcelRead.readNumericData(1, 1);
+	System.out.println(d);
+	}
 		
 
 
